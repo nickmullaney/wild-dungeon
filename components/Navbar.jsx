8e9bd7fe3b9ae7845d54b8f578b799a542562585
nickmullaney <nick.mullaney@gmail.com>
2024-01-19 09:13:54 -0800

    adding in base auth for test

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,3 @@
-'use client';
 import Link from 'next/link';
 import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 
@@ -12,7 +11,7 @@ const NavbarLink = ({ href, label }) => (
         backgroundColor: 'secondary.main', 
         color: 'white' 
       },
-      textTransform: 'none', // Keeps the original case of the text
+      textTransform: 'none',
       fontWeight: 'bold',
     }}
   >
@@ -20,23 +19,34 @@ const NavbarLink = ({ href, label }) => (
   </Button>
 );
 
-const Navbar = () => (
-  <AppBar position="static" color="primary">
-    <Toolbar>
-      <Typography variant="h4" component="div" sx={{ flexGrow: 1, fontWeight: 'bold' }}>
-        Wild Dungeon D&D
-      </Typography>
-      <Box sx={{ display: 'flex', gap: 1 }}>
-        <NavbarLink href="/" label="Home" />
-        <NavbarLink href="/about" label="About" />
-        <NavbarLink href="/campaign" label="Campaign" />
-        <NavbarLink href="/cities" label="Cities" />
-        <NavbarLink href="/characters" label="Character Vault" />
-        <NavbarLink href="/quests" label="Quests" />
-        <NavbarLink href="/encounters" label="Encounters" />
-      </Box>
-    </Toolbar>
-  </AppBar>
-);
+const Navbar = ({ isLoggedIn }) => { // Assuming isLoggedIn is passed as a prop
+  return (
+    <AppBar position="static" color="primary">
+      <Toolbar>
+        <Typography variant="h4" component="div" sx={{ flexGrow: 1, fontWeight: 'bold' }}>
+          Wild Dungeon D&D
+        </Typography>
+        <Box sx={{ display: 'flex', gap: 1 }}>
+          <NavbarLink href="/" label="Home" />
+          <NavbarLink href="/about" label="About" />
+          {isLoggedIn && (
+            <>
+              <NavbarLink href="/campaign" label="Campaign" />
+              <NavbarLink href="/cities" label="Cities" />
+              <NavbarLink href="/characters" label="Character Vault" />
+              <NavbarLink href="/quests" label="Quests" />
+              <NavbarLink href="/encounters" label="Encounters" />
+            </>
+          )}
+          {isLoggedIn ? (
+            <NavbarLink href="/logout" label="Logout" /> // Replace with your logout logic
+          ) : (
+            <NavbarLink href="/login" label="Login" /> // Replace with your login page link
+          )}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Navbar;
